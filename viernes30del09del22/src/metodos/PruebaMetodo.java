2e5ff6050718828cb5be35834d1ef3f0f65ab60e
AnwarBassett <abassettma@gmail.com>
2022-09-30 17:47:50 -0600

    Metodos

---
+++ viernes30del09del22/src/metodos/PruebaMetodo.java
@@ -0,0 +1,10 @@
+package metodos;
+
+public class PruebaMetodo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new IntroMetodos02().CalculadoraCuadratica();
+		
+
+	}
+}
